d195f3d47ab8fd4bdce07e9e64fef4b81f6c6fe3
Christopher Fernandes <christopher.webpg@gmail.com>
2025-02-07 01:05:56 -0400

    criado teste de remessa

--- tests/Unit/Services/RemessaProcessTest.php
+++ tests/Unit/Services/RemessaProcessTest.php
@@ -1,8 +1,31 @@
 <?php
 
-namespace Tests\Unit\Services;
+use PHPUnit\Framework\TestCase;
+use App\Services\DocumentProcess\RemessaProcess;
+use App\Constants\Messages;
 
-class RemessaProcessTest
+class RemessaProcessTest extends TestCase
 {
+    private RemessaProcess $remessaProcess;
 
+    protected function setUp(): void
+    {
+        $this->remessaProcess = new RemessaProcess();
+    }
+
+    public function testValidateThrowsExceptionWhenTituloDoesNotContainSemestre(): void
+    {
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage(Messages::REMESSA_ERRO_PROCESSO);
+
+        $documentDTO = ['titulo' => 'Processo de Remessa'];
+        $this->remessaProcess->validate($documentDTO);
+    }
+
+    public function testValidateDoesNotThrowExceptionWhenTituloContainsSemestre(): void
+    {
+        $documentDTO = ['titulo' => 'semestre de Processamento de Remessa'];
+        $this->remessaProcess->validate($documentDTO);
+        $this->assertTrue(true);
+    }
 }
